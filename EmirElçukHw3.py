7c4a6099262488ecec375f399ec1e994f13159fc
Emir Elçuk <123772472+emirelcuk@users.noreply.github.com>
2024-06-24 09:03:27 +0300

    Update EmirElçukHw3.py

--- EmirElçukHw3.py
+++ EmirElçukHw3.py
@@ -60,6 +60,5 @@ integer_list=[]
 for i in g_list: #user inputs list elements and the elements are converted to int value from char.
     integer_list.append(int(i))
 game(integer_list)
-if abc==1:
-    print("deneme")
+
 

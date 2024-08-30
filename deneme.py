156223d886c2bb9d30370d1c961a0ae754532be6
Emir Elçuk <123772472+emirelcuk@users.noreply.github.com>
2024-08-31 01:36:20 +0300

    TASK-11 Addsd

--- deneme.py
+++ deneme.py
@@ -1,1 +1,16 @@
+import random
 
+# Karakter sınıfı
+class Character:
+    def __init__(self, name, health, damage):
+        self.name = name
+        self.health = health
+        self.damage = damage
+
+    def attack(self, target):
+        damage_dealt = random.randint(1, self.damage)
+        target.health -= damage_dealt
+        print(f"{self.name} attacked {target.name} for {damage_dealt} damage.")
+
+    def is_alive(self):
+        return self.health > 0

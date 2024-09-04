36acf169d9e113ce995c70f5f93a056b810992a6
Emir Elçuk <123772472+emirelcuk@users.noreply.github.com>
2024-09-04 08:33:13 +0300

    TASK-11 AddsdS

--- deneme.py
+++ deneme.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
   
 @app.route('/', methods=['POST'])
-def webhook():
+def webhook(): 
     try:
         
         
